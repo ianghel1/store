8bf677f58fee14bde631d1a6adcce3163f5ddf0c
Valentin Ignat <archimonde90688@gmail.com>
2020-11-07 17:03:01 +0200

    metode crud

--- src/main/java/com/store/demo/controller/ProductController.java
+++ src/main/java/com/store/demo/controller/ProductController.java
@@ -1,13 +1,38 @@
 package com.store.demo.controller;
 
+import com.store.demo.model.Product;
 import com.store.demo.service.ProductService;
 import com.store.demo.service.ProductServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+@RequestMapping("product")
+@RestController
 public class ProductController {
 
     private final ProductService productService;
+
+
     public ProductController (ProductService productService){
         this.productService = productService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> add(@RequestBody Product product){
+
+        productService.add(product);
+        return ResponseEntity.noContent().build();
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> findById(@PathVariable Long id){
+        Optional<Product> queriedProduct = productService.findByID(id);
+       if(queriedProduct.isPresent()) {
+           return ResponseEntity.ok(queriedProduct.get());
+       }else{
+           return ResponseEntity.notFound().build();
+       }
     }
 
 }
